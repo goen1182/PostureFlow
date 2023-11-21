4abd7f7e1ac332672075c352c091fc87f3062c77
Nem <goen1182@gmail.com>
2023-11-22 07:42:49 +0900

    first commit

---
+++ src/firebase-config.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getAuth} from "firebase/auth";
+import {getDatabase} from 'firebase/database';
+import { getStorage } from "firebase/storage";
+
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_API_ID,
+  measurementId: process.env.REACT_APP_MESUREMNT_ID
+};
+
+
+const firebase  = initializeApp(firebaseConfig);
+const auth  = getAuth(firebase);
+
+// realtime data base
+const database = getDatabase(firebase);
+
+const storage = getStorage(firebase);
+
+
+export {firebase, auth, database, storage};
